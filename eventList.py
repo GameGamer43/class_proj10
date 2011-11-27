6b7cf8a6d6944c3fa236f728fcbd21a3d7d51d23
Bryan Walters <secretop@gmail.com>
2011-11-27 16:44:40 -0600

    add very simple doctests for the eventList class

--- eventList.py
+++ eventList.py
@@ -8,36 +8,50 @@ class EventList:
         constructor
         '''
         #list of events
-        self.__eList = []
+        self.eventList = []
 
     def insert(self, event):
         '''
         insert an event into the EventList
+        >>> e.insert(1)
+        ... print(e.eventList)
+        [1]
         '''
-        self.__eList.append(event)
+        self.eventList.append(event)
+        return self.eventList
 
     def pop(self, position = 0):
         '''
         method to pop an Event from the EventList and return the event.
+        >>> print(e.pop())
+        []
         '''
-        return self.__eList.pop(position)
+        self.eventList.pop(position)
+        return self.eventList
 
     def empty(self):
         '''
         return true if EventList is empty
+        >>> print(str(e.empty()))
+        True
         '''
-        if len(self.__eList) == 0:  
+        if len(self.eventList) == 0:  
             return True
         else:
             return False
 
     def __str__(self):
         """Represent the whole list as a string for printing -- very useful
-           during code development"""
+           during code development
+        """
         s = ""
-        for index, card in enumerate(self.__eList):
+        for index, value in enumerate(self.eventList):
             # insert newline: print 13 cards per line
             if index%13 == 0:
                 s += "\n"
-            s += str(card) + " "
+            s += str(value) + " "
         return s
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(extraglobs={'e': EventList()})
