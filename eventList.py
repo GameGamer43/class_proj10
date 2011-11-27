c59a26fd13c4497beb07017d71b24a85a1c76bda
Bryan Walters <secretop@gmail.com>
2011-11-26 23:53:35 -0600

    flush out more of the eventList class

--- eventList.py
+++ eventList.py
@@ -8,27 +8,36 @@ class EventList:
         constructor
         '''
         #list of events
-        self.eList = []
+        self.__eList = []
 
-    def insert(self, event = ''):
+    def insert(self, event):
         '''
         insert an event into the EventList
         '''
-        print(self)
-        pass
+        self.__eList.append(event)
 
     def pop(self, position = 0):
         '''
         method to pop an Event from the EventList and return the event.
         '''
-        pass
+        return self.__eList.pop(position)
 
-    
     def empty(self):
         '''
         return true if EventList is empty
         '''
-        if self.eList:
+        if len(self.__eList) == 0:  
             return True
         else:
             return False
+
+    def __str__(self):
+        """Represent the whole list as a string for printing -- very useful
+           during code development"""
+        s = ""
+        for index, card in enumerate(self.__eList):
+            # insert newline: print 13 cards per line
+            if index%13 == 0:
+                s += "\n"
+            s += str(card) + " "
+        return s
